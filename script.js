cac13a47acc5e0f276f0698ba924f094f3c37c29
namdery <namdery@mail.ru>
2026-01-15 17:57:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 // --- НАСТРОЙКИ ---
-const BOT_TOKEN = "ТВОЙ_ТОКЕН_БОТА";
+const BOT_TOKEN = "8567185651:AAFx8TIPf4nEle-hGT25sfip20dB7m0VT1I";
 const ADMIN_ID = "7632180689";
 
 // Фоновые изображения
